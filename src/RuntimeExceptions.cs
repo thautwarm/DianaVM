c7f1fa35dda1ea702a7d14928aa5bb48d903d098
thautwarm <twshere@outlook.com>
2021-09-25 15:06:41 +0900

    stage

--- src/RuntimeExceptions.cs
+++ src/RuntimeExceptions.cs
@@ -3,11 +3,11 @@ namespace DianaScript
 {
 
 
-public class D_AttributeError : Exception, DObj
+public class D_AttributeError : Exception
 {
     public D_AttributeError(DClsObj t, DStr attr, string message) : base(message){
     }
-    public D_AttributeError(DClsObj t, DStr attr) : base($"{t.Repr} has no attribute {attr.value}.")
+    public D_AttributeError(DClsObj t, DStr attr) : base($"{t.__repr__} has no attribute {attr.value}.")
     {
     }
     public D_AttributeError(string message): base(message)
@@ -17,11 +17,11 @@ public class D_AttributeError : Exception, DObj
     
 }
 
-public class D_TypeError : Exception, DObj
+public class D_TypeError : Exception
 {
     public DClsObj expect;
     public DObj got;
-    public D_TypeError(DClsObj expect, DObj o): base($"expect an instance of {expect.Repr}, but got {o.Repr}.")
+    public D_TypeError(DClsObj expect, DObj o): base($"expect an instance of {expect.__repr__}, but got {o.__repr__}.")
     {
     }
 
@@ -38,9 +38,9 @@ public class D_TypeError : Exception, DObj
     }
 }
 
-public class D_ValueError : Exception, DObj
+public class D_ValueError : Exception
 {
-    public D_ValueError(DObj expect, DObj o): base($"expect the value {expect.Repr}, but got {o.Repr}.")
+    public D_ValueError(DObj expect, DObj o): base($"expect the value {expect.__repr__}, but got {o.__repr__}.")
     {
     }
     public D_ValueError(DObj expect, DObj o, string message): base(message)
